e5baee508fcb5145c0a58a8c4113b390fa033a88
Avinash9807 <82309253+avinash9807@users.noreply.github.com>
2025-12-27 19:13:39 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# --- CONFIGURATION FILE ---
+API_ID =  12345576 
+API_HASH = 'jsjskkskkamaabna'     
+BOT_TOKEN = '8363839:AA-hejskkhwbsj4'   
+OWNERS = [82049393018, 738383837]
+# --------------------------
